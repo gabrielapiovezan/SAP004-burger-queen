42fa42d0371f08059c98b4aa47f8788849a0cd95
Mariana Barros <mamirandabarros11@gmail.com>
2020-08-01 15:08:31 -0300

    kitchen

--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../img/logo1.png";
 import Button from "../../components/Button";
 import Command from "../../components/Command";
 import "./style.css";
 
 const Kitchen = () => {
+  const [requests, setRequests] = useState([{
+    name: 'ada',
+    table: "01",
+    calendar: "25/02/2021",
+    schedule: "12:00",
+    itens: [{
+      product: "água",
+      quant: "1"
+    }, {
+      product: "água",
+      quant: "1"
+    }]
+  }, {
+    name: 'ada',
+    table: "01",
+    calendar: "25/02/2021",
+    schedule: "12:00",
+    itens: [{
+      product: "água",
+      quant: "1"
+    }, {
+      product: "água",
+      quant: "1"
+    }]
+  }]);
   return (
     <div className="container">
       <div className="kitchen">
         <img className="img-kitchen" src={logo} alt="logo" />
         <Button value="Pedidos" />
       </div>
-      <Command pedido={{
-        name: 'ada',
-        table: "01",
-        calendar: "25/02/2021",
-        schedule: "12:00",
-        itens: [{
-          product: "água",
-          quant: "1"
-        }, {
-          product: "água",
-          quant: "1"
-        }]
-      }} />
+      <div className="request">
+        {requests.map(request =>
+          <Command request={request} />
+        )}
+      </div>
     </div>
   )
 };
