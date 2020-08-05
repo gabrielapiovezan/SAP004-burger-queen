f4afb2de87572f0a17a585b717a6848624980f31
Mariana Barros <mamirandabarros11@gmail.com>
2020-08-05 11:57:52 -0300

    command

--- src/pages/Kitchen/Index.jsx
+++ src/pages/Kitchen/Index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "../../img/logo1.png";
 import Button from "../../components/Button";
 import Modal from "../../components/Modal/Index";
 import Command from "../../components/Command/Index";
@@ -36,10 +35,6 @@ const Kitchen = () => {
 
   return (
     <div className="container">
-      <div className="kitchen">
-        <img className="img-kitchen" src={logo} alt="logo" />
-        <Button value="Voltar" />
-      </div>
       <div className="request" >
         {requests.map(request =>
           <Command key={request.id} request={request} onClick={() => handleModal(request)} />
