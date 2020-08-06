3c57492ef9b4fa25ba090dc8182ce8c77773e232
Mariana Barros <mamirandabarros11@gmail.com>
2020-08-05 22:40:30 -0300

    history

--- src/pages/OrderHistory/Index.jsx
+++ src/pages/OrderHistory/Index.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Button from "../../components/Button";
+import logo from "../../img/logo1.png";
 import Command from "../../components/Command/Index";
 import { getData } from "../../firebase/firebaseService";
 import "./style.css";
@@ -13,9 +15,16 @@ const OrderHistory = () => {
     getData(get)
   }, []);
 
+  const onClickBack = async () => {
+
+  };
 
   return (
     <div className="container">
+      <div className="container-history">
+        <img className="img-history" src={logo} alt="logo" />
+        <Button className="button button-history" value="Voltar" onClick={onClickBack} />
+      </div>
       <div className="request" >
         {requests.map(request =>
           <Command request={request} />
