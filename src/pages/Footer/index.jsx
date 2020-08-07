d040554e223e1c510805bcbcce043c3ae54becd9
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-08-07 20:18:59 -0300

    obs

---
+++ src/pages/Footer/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return <footer className="footer">oi</footer>;
+};
+export default Footer;
+
+// import React from "react";
+// import "./style.css";
+
+// // class Button extends React.Component {
+// //   render() {
+// const Button = (props) => {
+//   return (
+//     <button className="button " {...props}>
+//       {props.value}
+//     </button> //onClick={this.props.onClick}>{this.props.value}
+//   );
+// };
+// //}
+
+// export default Button;
