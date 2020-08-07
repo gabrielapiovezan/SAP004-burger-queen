2f20c6fae3ec72fd2cb5ea32184d01c7913c9157
Mariana Barros <mamirandabarros11@gmail.com>
2020-08-06 23:31:36 -0300

    alert

--- src/pages/authPages/Register.jsx
+++ src/pages/authPages/Register.jsx
@@ -4,7 +4,7 @@ import { authRegister } from "../../firebase/authService";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import ReturnError from "./authError";
-import Notify from "../../components/Notify/Index";
+import { toast } from 'react-toastify';
 import logo from "../../img/logo1.png";
 import chapeu1 from "../../img/chapeu-1.png";
 import chapeu2 from "../../img/chapeu-2.png";
@@ -28,9 +28,9 @@ const Register = () => {
     } else {
       try {
         const response = await authRegister({ name, email, password, type });
-
+        toast.success("Cadastro realizado com sucesso!");
         // alert("cadastro realizado com sucesso");
-        // history.push("/login");
+        history.push("/login");
       } catch (error) {
         setError(error.code);
       }
