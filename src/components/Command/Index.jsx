a59601f22f1bf26813cb27f4469ef82f6ebfc344
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-08-08 05:47:00 -0300

    footer

--- src/components/Command/Index.jsx
+++ src/components/Command/Index.jsx
@@ -4,13 +4,12 @@ import "./style.css";
 const Command = ({ request, onClick }) => {
   const dateAndHour = (date) => {
     if (!date) {
-      return ""
+      return "";
     }
-    const options = { dateStyle: 'short', timeStyle: 'short' };
-    return date.toDate().toLocaleDateString('pt-BR', options);
+    const options = { dateStyle: "short", timeStyle: "short" };
+    return date.toDate().toLocaleDateString("pt-BR", options);
   };
   return (
-
     <div className="container-command" onClick={onClick}>
       <div className="data-command">
         <span>Nome: {request.value[0].name}</span>
@@ -21,28 +20,29 @@ const Command = ({ request, onClick }) => {
       </div>
       <div className="command">
         <ul>
-          {request.value.map((prod, index) =>
+          {request.value.map((prod, index) => (
             <li key={index}>
               <span className="item-command">
                 {prod.amount} {prod.item} <br></br>
-                {prod.burguer ? prod.burguer.reduce((burguerPreview, burguerNext, index) => {
-                  return burguerPreview += ` ${burguerNext} ${prod.option[index]}`
-                }, "") : ""}
+                {prod.burguer
+                  ? prod.burguer.reduce(
+                      (burguerPreview, burguerNext, index) => {
+                        return (burguerPreview += ` ${burguerNext} ${prod.option[index]}`);
+                      },
+                      ""
+                    )
+                  : ""}
               </span>
-              <span>
-                R${prod.price}
-              </span>
+              <span>R${prod.price}</span>
             </li>
-          )}
+          ))}
         </ul>
         <div className="result">
-          <span>Total:  R${request.value[0].total}</span>
+          <span>Total: R${request.value[0].total}</span>
         </div>
       </div>
     </div>
-
   );
 };
-
 
 export default Command;
--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -2,22 +2,10 @@ import React from "react";
 import "./style.css";
 
 const Footer = () => {
-  return <footer className="footer">oi</footer>;
+  return (
+    <footer className="footer">
+      Desenvolvido por Gabriela Piovezan e Mariana Barros
+    </footer>
+  );
 };
 export default Footer;
-
-// import React from "react";
-// import "./style.css";
-
-// // class Button extends React.Component {
-// //   render() {
-// const Button = (props) => {
-//   return (
-//     <button className="button " {...props}>
-//       {props.value}
-//     </button> //onClick={this.props.onClick}>{this.props.value}
-//   );
-// };
-// //}
-
-// export default Button;
